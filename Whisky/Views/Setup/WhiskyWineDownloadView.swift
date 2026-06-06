91de7a2153bebba924b2c9b83420b5c3c2a08a1a
mflr0 <mflr0@users.noreply.github.com>
2026-06-06 19:00:43 +0200

    Add GitHub Actions workflow for packaging WhiskyWine and update download URLs

--- Whisky/Views/Setup/WhiskyWineDownloadView.swift
+++ Whisky/Views/Setup/WhiskyWineDownloadView.swift
@@ -65,7 +65,8 @@ struct WhiskyWineDownloadView: View {
         .frame(width: 400, height: 200)
         .onAppear {
             Task {
-                if let url: URL = URL(string: "https://data.getwhisky.app/Wine/Libraries.tar.gz") {
+                let wineURL = "https://github.com/0xGuigui/Whisky/releases/latest/download/Libraries.tar.gz"
+                if let url: URL = URL(string: wineURL) {
                     downloadTask = URLSession(configuration: .ephemeral).downloadTask(with: url) { url, _, _ in
                         Task.detached {
                             await MainActor.run {
