0da4f8b7d64323e98518934d395fbf8e648759eb
mflr0 <mflr0@onionmail.org>
2026-06-06 19:00:43 +0200

    Add GitHub Actions workflow for packaging WhiskyWine and update download URLs

--- WhiskyKit/Sources/WhiskyKit/WhiskyWine/WhiskyWineInstaller.swift
+++ WhiskyKit/Sources/WhiskyKit/WhiskyWine/WhiskyWineInstaller.swift
@@ -61,7 +61,7 @@ public class WhiskyWineInstaller {
     }
 
     public static func shouldUpdateWhiskyWine() async -> (Bool, SemanticVersion) {
-        let versionPlistURL = "https://data.getwhisky.app/Wine/WhiskyWineVersion.plist"
+        let versionPlistURL = "https://github.com/0xGuigui/Whisky/releases/latest/download/WhiskyWineVersion.plist"
         let localVersion = whiskyWineVersion()
 
         var remoteVersion: SemanticVersion?
